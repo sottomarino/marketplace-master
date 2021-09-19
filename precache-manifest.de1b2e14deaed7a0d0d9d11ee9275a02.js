ec6c4af51b8a98484d89d61dd3eb1ec6de08e6ff
sottomarino <simone.ambrosi@live.com>
2021-09-19 19:10:41 +0200

    Updates

---
+++ precache-manifest.de1b2e14deaed7a0d0d9d11ee9275a02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b70fcec63bfd607cf9c2",
+    "url": "/marketplace-master/static/css/main.52af30ba.chunk.css"
+  },
+  {
+    "revision": "b70fcec63bfd607cf9c2",
+    "url": "/marketplace-master/static/js/main.b70fcec6.chunk.js"
+  },
+  {
+    "revision": "d30128737513a06ae5c2",
+    "url": "/marketplace-master/static/css/1.7c4abf0f.chunk.css"
+  },
+  {
+    "revision": "d30128737513a06ae5c2",
+    "url": "/marketplace-master/static/js/1.d3012873.chunk.js"
+  },
+  {
+    "revision": "b6091106bbbf723ec4fb",
+    "url": "/marketplace-master/static/js/runtime~main.b6091106.js"
+  },
+  {
+    "revision": "0682563374c57f7b251d9ad5ba982327",
+    "url": "/marketplace-master/static/media/logo.06825633.png"
+  },
+  {
+    "revision": "fb5339bfa22e3ad129f97697468eb96b",
+    "url": "/marketplace-master/index.html"
+  }
+];
